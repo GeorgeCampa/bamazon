57b5c74e345ca22afc0560a0a655bf3fffdf42d0
George <Jc82200172@gmail.com>
2019-07-24 01:04:15 -0700

    1st commit

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,1 +1,43 @@
-bamazon
+var mysql = require('mysql');
+var inquirer = ("inquirer");
+var Table = require("cli-table2");
+
+var connection = mysql.createConnection({
+    host: '127.0.0.1',
+    port: '3306',
+    user: 'root',
+    password: 'Applegood99',
+    database: 'bamazon_db'
+});
+
+connection.connect();
+
+var display = function() {
+  connection.query("SELECT * FROM products", function(err, res, fie) {
+    if (err) throw err;
+    console.log("-----------------------------");
+    console.log("      Welcome To Bamazon    ");
+    console.log("-----------------------------");
+    console.log("");
+    console.log("Find below our Products List");
+    console.log("");
+    var table = new Table({
+      head: ["Product Id", "Product Description", "Cost"],
+      colWidths: [12, 50, 8],
+      colAligns: ["center", "left", "right"],
+      style: {
+        head: ["aqua"],
+        compact: true
+      }
+    });
+
+    for (var i = 0; i < res.length; i++) {
+      table.push([res[i].id, res[i].products_name, res[i].price]);
+    }
+
+    console.log(table.toString());
+    console.log("");
+  }); 
+};
+
+display();
